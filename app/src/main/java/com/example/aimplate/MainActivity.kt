c6a082d27c16137dc0d73dfe3d810e03a795f1c2
ruiduart <ruysilva5@gmail.com>
2022-11-18 23:06:53 +0000

    Added button functionality and counter var

--- app/src/main/java/com/example/aimplate/MainActivity.kt
+++ app/src/main/java/com/example/aimplate/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.aimplate
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 import com.example.aimplate.databinding.ActivityMainBinding
+
+object counter {
+    var count : Int = 0
+}
 
 class MainActivity : Activity() {
 
@@ -14,6 +19,11 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    fun addCounter(view: View) {
+        counter.count++
+        println(counter.count)
     }
 }
 
