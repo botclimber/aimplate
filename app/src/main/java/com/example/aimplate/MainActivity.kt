4f0c2fa2d715d2714c17ac7c6faaf43ef73ed9d0
Daniel Silva <daniel.silva.prg@gmail.com>
2022-11-23 00:08:34 +0000

    wip: please dont touch this branch

--- app/src/main/java/com/example/aimplate/MainActivity.kt
+++ app/src/main/java/com/example/aimplate/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.aimplate
 
 import android.app.Activity
+import android.hardware.*
 import android.os.Bundle
-import android.view.View
+import android.widget.TextView
 import com.example.aimplate.databinding.ActivityMainBinding
+import android.graphics.Color
+import android.view.WindowManager
 
-object counter {
-    var count : Int = 0
-}
 
-class MainActivity : Activity() {
+class MainActivity : Activity(), SensorEventListener {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var sensorManager: SensorManager
+    private lateinit var square: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +21,61 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //AppComatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        square = findViewById(R.id.dSquare)
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setUpSensorStuff()
     }
 
-    fun addCounter(view: View) {
-        counter.count++
-        println(counter.count)
+    private fun setUpSensorStuff() {
+        // Create the sensor manager
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+
+        // Specify the sensor you want to listen to
+        sensorManager.getDefaultSensor(Sensor.)?.also { accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
     }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        // Checks for the sensor we have registered
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            //Log.d("Main", "onSensorChanged: sides ${event.values[0]} front/back ${event.values[1]} ")
+
+            // Sides = Tilting phone left(10) and right(-10)
+            val sides = event.values[0]
+
+            // Up/Down = Tilting phone up(10), flat (0), upside-down(-10)
+            val upDown = event.values[1]
+
+            square.apply {
+                rotationX = upDown * 3f
+                rotationY = sides * 3f
+                rotation = -sides
+                translationX = sides * -10
+                translationY = upDown * 10
+            }
+
+            // Changes the colour of the square if it's completely flat
+            val color = if (upDown.toInt() == 0 && sides.toInt() == 0) Color.GREEN else Color.RED
+            square.setBackgroundColor(color)
+
+            square.text = "up/down ${upDown.toInt()}\nleft/right ${sides.toInt()}"
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
+    }
+
 }
-
